6dc68768d5e4a58d71b5ec7ece114df1861006f0
Eugene Chevski <eugenechevski@proton.me>
2024-02-22 22:26:35 -0500

    updates two-pointers/3Sum

--- two-pointers/3Sum/solution.py
+++ two-pointers/3Sum/solution.py
@@ -1,64 +1,29 @@
-"""
-https://github.com/eugenechevski
-https://leetcode.com/problems/3sum/
-
-Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] 
-such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
-Notice that the solution set must not contain duplicate triplets.
-
-Example 1:
-    Input: nums = [-1,0,1,2,-1,-4]
-    Output: [[-1,-1,2],[-1,0,1]]
-
-Example 2:
-    Input: nums = []
-    Output: []
-
-Example 3:
-    Input: nums = [0]
-    Output: []
- 
-Constraints:
-    0 <= nums.length <= 3000
-    -10^5 <= nums[i] <= 10^5
-"""
-
-from asyncio import constants
-from copy import copy
+from collections import defaultdict
 
 
-class Solution(object):
-    def threeSum(self, nums):
-        sorted_nums = sorted(nums)
-        if len(sorted_nums) < 3 or sorted_nums[0] ^ sorted_nums[-1] > 0:
-            return []
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums = sorted(nums)
+        result = []
 
-        triplets = set()
-        l = 0
-        hash_set = set()
-        while sorted_nums[l] < 0:
-            r = len(sorted_nums) - 1
-            third_value = 0 - (sorted_nums[l] + sorted_nums[r])
+        for i in range(len(nums)):
+            # Skip the duplicates
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
 
-            while third_value <= sorted_nums[-1] and l < r:
-                if third_value in hash_set:
-                    triplet = tuple(
-                        sorted([sorted_nums[l], third_value, sorted_nums[r]]))
-                    if triplet not in triplets:
-                        triplets.add(triplet)
+            l, r = i + 1, len(nums) - 1
+
+            while l < r:
+                threeSum = nums[i] + nums[l] + nums[r]
+
+                if threeSum < 0:
+                    l += 1
+                elif threeSum > 0:
+                    r -= 1
                 else:
-                    hash_set.add(sorted_nums[r])
+                    result.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
 
-                r -= 1
-                third_value = 0 - (sorted_nums[l] + sorted_nums[r])
-
-            hash_set = set()
-            l += 1
-
-        if sorted_nums[l] == 0 \
-           and l + 2 < len(sorted_nums) \
-           and sorted_nums[l + 2] == 0:
-            triplets.add(tuple([0, 0, 0]))
-
-        return list(triplets)
-        
+        return result
